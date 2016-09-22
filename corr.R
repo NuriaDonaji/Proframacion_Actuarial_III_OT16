f043aa11cc7d9e4411cb7df7b73da53bbd0c8587
NuriaDonaji <nuria_123@live.com.mx>
2016-09-21 20:14:03 -0500

    Update corr.R

--- corr.R
+++ corr.R
@@ -1,21 +1,9 @@
 corr <- function(directorio,horizonte = 0)
-  cor()
-
-mediacontaminante <- function(directorio,contaminante = sulfate/nitrate,id = 1:332)
 {setwd("~/GitHub/Programacion_Actuarial_III_OT16/")
   tabla <- vector("numeric")
-  for (i in id){
-    
+  for (i in 1:332)
     tabla <- read.csv(paste(directorio,"/",formatC(i,width = 3, flag= "0"),".csv", sep = ""))
-    
     completos <- complete.cases(tabla)
     t <- tabla[completos,]
-    tt <- rbind(t[completos,])
-  }
-  if (contaminante== "sulfate"){
-    media <- mean(t[,2]) 
-  }else if(contaminante==nitrate)
-  {media <- mean(tt[,3])} 
-  media}
-
-mediacontaminante("specdata","sulfate",1:2)
+    
+mediacontaminante("specdata",0)
