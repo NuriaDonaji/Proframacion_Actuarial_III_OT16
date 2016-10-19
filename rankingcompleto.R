f82068abe1a59e5224f24841a15070235a50fee1
NuriaDonaji <nuria_123@live.com.mx>
2016-10-18 21:54:29 -0500

    Caso 2

--- rankingcompleto.R
+++ rankingcompleto.R
@@ -24,9 +24,10 @@ estado <-estados[order(estados)]
 l <- length(estados)
 g<- vector()
 for (i in 1:l) {
-  a <- outcome[outcome$State == estado[i],]
+  a <- outcome[grep(estado[i],outcome$State),]
   b <- a[,col]
   Hospital <- a[,2]
+  hospital <- as.character(Hospital)
   d <- cbind(Hospital,b)
   e<-suppressWarnings(as.numeric(d[,2]))
   orden<-suppressWarnings(order(e,na.last = NA))
@@ -37,10 +38,10 @@ for (i in 1:l) {
     }else if (num=="peor"){
         f <- tail(orden2)[6]
     }else {
-        f <-orden2[num-1]
+        f <-orden2[num]
     }
   
- g <- append(g, c(g, estado[i])) 
+ g <- append(g, c(f, estado[i])) 
 }
 
 # Regresa un data frame con el nombre del hospital y la abreviatura del nombre del estado al que pertenece. 
@@ -51,6 +52,6 @@ colnames(g) = c("Hospital", "Estado")
 
 g
 }
-head(rankingcompleto("ataque"), 20)
+head(rankingcompleto("ataque",20), 10)
 tail(rankingcompleto("neumonia", "peor"), 3)
 tail(rankingcompleto("falla"), 10)
